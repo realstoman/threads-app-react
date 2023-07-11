13c4a04de66dfb432d4deba5415bb278dd313655
realstoman <realstoman@gmail.com>
2023-07-11 17:18:21 +0430

    Create pages

---
+++ app/activity.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const activity = () => {
+	return <div>activity</div>;
+};
+
+export default activity;
---
+++ app/search.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const search = () => {
+	return <div>search</div>;
+};
+
+export default search;
