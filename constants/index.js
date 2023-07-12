621194ce12b2a985000a69933f8e41a24ba3b5ba
realstoman <realstoman@gmail.com>
2023-07-12 16:11:31 +0430

    Constants

---
+++ constants/index.js
@@ -0,0 +1,13 @@
+import HamzaYusufAvatar from '../public/avatars/hamza-yusuf-avatar.png';
+import BlueCheckmark from '../public/checkmarks/blue-checkmark.png';
+
+const accountsToFollow = [
+	{
+		id: 1,
+		fullName: 'Hamza Yusuf',
+		username: 'hamzayusuf',
+		followersCount: '2m',
+		avatarUrl: HamzaYusufAvatar,
+		verified: BlueCheckmark,
+	},
+];
