781a87f31ee5fe7d7f9764a5a4a80f5e2b95f4e7
realstoman <realstoman@gmail.com>
2023-07-13 22:42:28 +0430

    Reposted props and title

--- components/Post.tsx
+++ components/Post.tsx
@@ -19,6 +19,8 @@ interface PostType {
 	publishTime: string;
 	mentions?: string;
 	isLiked?: boolean;
+	isReposted?: boolean;
+	repostedBy?: string;
 	carousel?: [] & any;
 	totalReplies?: string;
 	totalLikes?: string;
@@ -32,107 +34,119 @@ const Post = ({
 	publishTime,
 	mentions,
 	isLiked,
+	isReposted,
+	repostedBy,
 	carousel,
 	totalReplies,
 	totalLikes,
 }: PostType) => {
 	return (
-		<div className="flex px-4 gap-3 my-4 w-full font-sans">
-			<div className="flex flex-col w-12 sm:w-14 min-h-full justify-between items-center shrink-0 p-0 m-0">
+		<div className="px-4 my-4 w-full font-sans">
+			{isReposted ? (
 				<div className="">
-					<Image
-						src={avatar}
-						width={40}
-						height={40}
-						alt="Logo"
-						className="rounded-full"
-					/>
+					<FiRepeat className="text-gray-100  -rotate-12 sm:text-xl" />
+					<span>{repostedBy} reposted</span>
 				</div>
-				<div className="w-full flex flex-col justify-center items-center">
-					<Image
-						src={ZuckAvatar}
-						width={16}
-						height={16}
-						alt="Logo"
-						className="rounded-full"
-					/>
-					<Image
-						src={ZuckAvatar}
-						width={12}
-						height={12}
-						alt="Logo"
-						className="rounded-full"
-					/>
-					<Image
-						src={ZuckAvatar}
-						width={8}
-						height={8}
-						alt="Logo"
-						className="rounded-full"
-					/>
-				</div>
-			</div>
-			<div className="flex flex-col w-full">
-				<div className="flex items-center justify-between">
-					<div className="flex items-center gap-2 w-full">
-						<p className="text-md sm:text-xl font-medium">
-							{username}
-						</p>
-						{isVerified ? (
-							<Image
-								src={BlueCheckmark}
-								width={14}
-								height={14}
-								alt="Blue Checkmark"
-								className="rounded-full w-4 sm:w-5"
-							/>
-						) : (
-							''
-						)}
+			) : (
+				''
+			)}
+			<div className="flex justify-start gap-3">
+				<div className="flex flex-col w-12 sm:w-14 min-h-full justify-between items-center shrink-0 p-0 m-0">
+					<div className="">
+						<Image
+							src={avatar}
+							width={40}
+							height={40}
+							alt="Logo"
+							className="rounded-full"
+						/>
 					</div>
-					<div className="flex items-center gap-3">
-						<span className="text-xs sm:text-sm text-gray-500">
-							{publishTime}
-						</span>
-						<a href="#">
-							<FiMoreHorizontal className="text-gray-100" />
-						</a>
+					<div className="w-full flex flex-col justify-center items-center">
+						<Image
+							src={ZuckAvatar}
+							width={16}
+							height={16}
+							alt="Logo"
+							className="rounded-full"
+						/>
+						<Image
+							src={ZuckAvatar}
+							width={12}
+							height={12}
+							alt="Logo"
+							className="rounded-full"
+						/>
+						<Image
+							src={ZuckAvatar}
+							width={8}
+							height={8}
+							alt="Logo"
+							className="rounded-full"
+						/>
 					</div>
 				</div>
+				<div className="flex flex-col w-full">
+					<div className="flex items-center justify-between">
+						<div className="flex items-center gap-2 w-full">
+							<p className="text-md sm:text-xl font-medium">
+								{username}
+							</p>
+							{isVerified ? (
+								<Image
+									src={BlueCheckmark}
+									width={14}
+									height={14}
+									alt="Blue Checkmark"
+									className="rounded-full w-4 sm:w-5"
+								/>
+							) : (
+								''
+							)}
+						</div>
+						<div className="flex items-center gap-3">
+							<span className="text-xs sm:text-sm text-gray-500">
+								{publishTime}
+							</span>
+							<a href="#">
+								<FiMoreHorizontal className="text-gray-100" />
+							</a>
+						</div>
+					</div>
 
-				<div className="">
-					<div className="mt-1.5 sm:mt-2">
-						<p className="text-xs sm:text-sm">{postContent}</p>
-						<Link href="/" className="text-blue-400">
-							{mentions}
-						</Link>
-						<div className="mt-2">{carousel}</div>
-					</div>
+					<div className="">
+						<div className="mt-1.5 sm:mt-2">
+							<p className="text-xs sm:text-sm">{postContent}</p>
+							<Link href="/" className="text-blue-400">
+								{mentions}
+							</Link>
+							<div className="mt-2">{carousel}</div>
+						</div>
 
-					<div className="flex gap-4 mt-3 sm:mt-4">
-						<button type="button">
-							<FiHeart
-								className={
-									isLiked
-										? 'fill-red-600 text-red-600 sm:text-xl'
-										: 'fill-none text-gray-100 sm:text-xl'
-								}
-							/>
-						</button>
-						<button type="button">
-							<FiMessageCircle className="text-gray-100 -rotate-90 sm:text-xl" />
-						</button>
-						<button type="button">
-							<FiRepeat className="text-gray-100  -rotate-12 sm:text-xl" />
-						</button>
-						<button type="button">
-							<FiNavigation className="text-gray-100 sm:text-xl" />
-						</button>
-					</div>
-					<div className="flex items-start gap-2 text-gray-500 mt-4 text-xs sm:text-[14px] text-center">
-						<p>{totalReplies} replies</p>
-						<span>.</span>
-						<p>{totalLikes} likes</p>
+						<div className="flex gap-4 mt-3 sm:mt-4">
+							<button type="button">
+								<FiHeart
+									className={
+										isLiked
+											? 'fill-red-600 text-red-600 sm:text-xl'
+											: 'fill-none text-gray-100 sm:text-xl'
+									}
+								/>
+							</button>
+							<button type="button">
+								<FiMessageCircle className="text-gray-100 -rotate-90 sm:text-xl" />
+							</button>
+							<button type="button">
+								<FiRepeat className="text-gray-100  -rotate-12 sm:text-xl" />
+							</button>
+							<button type="button">
+								<FiNavigation className="text-gray-100 sm:text-xl" />
+							</button>
+						</div>
+						<div className="flex items-start gap-2 text-gray-500 mt-4 text-xs sm:text-[14px] text-center">
+							<p>{totalReplies} replies</p>
+							<span>.</span>
+							<p>{totalLikes} likes</p>
+						</div>
 					</div>
 				</div>
 			</div>
