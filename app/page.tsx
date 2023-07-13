7b686585559e5888996ed24304f4d539d5a0ebfa
realstoman <realstoman@gmail.com>
2023-07-13 17:27:06 +0430

    Post initial ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,103 @@
 'use client';
 import Image from 'next/image';
-import Logo from '../public/logo/logo.svg';
+import Logo from '@/public/logo/logo.svg';
+import StomanAvatar from '@/public/avatars/stoman-avatar.jpg';
+import ZuckAvatar from '@/public/avatars/zuck-avatar.jpg';
+import BlueCheckmark from '@/public/avatars/blue-checkmark.png';
+import {
+	FiHeart,
+	FiMessageCircle,
+	FiMoreHorizontal,
+	FiNavigation,
+	FiRepeat,
+} from 'react-icons/fi';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-10">
+		<main className="flex min-h-screen flex-col items-center justify-between py-5">
 			<header className="">
-				<Image src={Logo} width={100} height={100} alt="Logo" />
+				<Image src={Logo} width={50} height={50} alt="Logo" />
 
 				<div className="main-content">
 					{/* Single Post */}
-					<div className="post">
-						<div className="users">
-							<div className="author">
-								<img
-									src="./public/avatars/zuck-avatar.jpg"
-									alt="Author"
+					<div className="flex w-full px-4 gap-6">
+						<div className="flex flex-col w-20 h-full justify-between">
+							<div className="flex flex-col">
+								<Image
+									src={ZuckAvatar}
+									width={45}
+									height={45}
+									alt="Logo"
+									className="rounded-full"
 								/>
 							</div>
-							<div className="others">
-								<img
-									src="./public/avatars/stoman-avatar.jpg"
-									alt="User 1"
-									width="16"
+							<div className="flex text-left">
+								<Image
+									src={ZuckAvatar}
+									width={22}
+									height={22}
+									alt="Logo"
+									className="rounded-full"
 								/>
-								<img
-									src="./public/avatars/avatar-1.jpeg"
-									alt="User 1"
-									width="13"
+								<Image
+									src={ZuckAvatar}
+									width={16}
+									height={16}
+									alt="Logo"
+									className="rounded-full"
 								/>
-								<img
-									src="./public/avatars/avatar-4.jpg"
-									alt="User 1"
-									width="12"
+								<Image
+									src={ZuckAvatar}
+									width={14}
+									height={14}
+									alt="Logo"
+									className="rounded-full"
 								/>
 							</div>
 						</div>
-						<div className="contents">
-							<div className="top">
-								<div className="if-verified">
-									<p>zuck</p>
-									<img
-										src="./public/avatars/blue-checkmark.png"
-										alt=""
+						<div className="flex flex-col">
+							<div className="flex items-start justify-between">
+								<div className="flex items-center gap-2 w-full">
+									<p className="text-lg">zuck</p>
+									<Image
+										src={BlueCheckmark}
+										width={14}
+										height={14}
+										alt="Logo"
+										className="rounded-full"
 									/>
 								</div>
-								<div className="timeAndShare">
-									<span>10m</span>
+								<div className="flex items-center gap-3">
+									<span className="text-sm text-gray-500">
+										10m
+									</span>
 									<a href="#">
-										<i data-feather="more-horizontal"></i>
+										<FiMoreHorizontal className="text-gray-500" />
 									</a>
 								</div>
 							</div>
 
-							<div className="bottom">
-								<div className="content">
-									<p>
-										70 million sign ups on Threads as of
-										this morning. Way beyond our
-										expectations.
-									</p>
+							<div className="">
+								<div className="text-sm mt-2">
+									70 million sign ups on Threads as of this
+									morning. Way beyond our expectations.
 								</div>
 
-								<div className="cta">
+								<div className="flex gap-4 mt-4">
 									<a href="#">
-										<i
-											data-feather="heart"
-											className="filled"
-										></i>
+										<FiHeart className="fill-red-600 text-red-600" />
 									</a>
 									<a href="#">
-										<i
-											data-feather="message-circle"
-											className="rotateComment"
-										></i>
+										<FiMessageCircle className="text-gray-500" />
 									</a>
 									<a href="#">
-										<i
-											data-feather="repeat"
-											className="rotateRepost"
-										></i>
+										<FiRepeat className="text-gray-500" />
 									</a>
 									<a href="#">
-										<i data-feather="navigation"></i>
+										<FiNavigation className="text-gray-500" />
 									</a>
 								</div>
-								<div className="counts">
+								<div className="flex items-start gap-4 text-gray-500 mt-2 text-sm">
 									<div className="reply">
 										<p>18,257 replies</p>
 									</div>
