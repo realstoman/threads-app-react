d6b60f519c214c313be7dfa72c80339ef71a7e4a
realstoman <realstoman@gmail.com>
2023-07-14 15:24:31 +0430

    Add footer layout and content

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import PageWrapper from '@/components/PageWrapper';
 
 const search = () => {
--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,6 +1,6 @@
 const PageWrapper = ({ children }: any) => {
 	return (
-		<main className="mx-auto min-h-screen max-w-lg sm:max-w-4xl font-sans">
+		<main className="mx-auto max-w-lg sm:max-w-4xl font-sans">
 			{children}
 		</main>
 	);
