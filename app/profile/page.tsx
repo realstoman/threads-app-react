d69b47b3ea0c5214be63d812b88f1db4db57d648
realstoman <realstoman@gmail.com>
2023-07-14 19:17:44 +0430

    Profile cta buttons

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,6 +4,9 @@ import PageWrapper from '@/components/PageWrapper';
 import Image from 'next/image';
 import { FiGlobe, FiInstagram, FiSettings } from 'react-icons/fi';
 import StomanProfile from '@/public/avatars/stoman-avatar.jpg';
+import Avatar1 from '@/public/avatars/avatar-1.jpeg';
+import NikeLogo from '@/public/logo/nike-logo.jpg';
+import RonaldoAvatar from '@/public/avatars/ronaldo-avatar.jpg';
 
 const profile = () => {
 	return (
@@ -48,35 +51,35 @@ const profile = () => {
 							/>
 						</div>
 
-						<p className="text-sm text-gray-300">
+						<p className="text-xs text-gray-300">
 							Talks about programming, philosophy, design. Always
 							a student. Working on something new.
 						</p>
 					</div>
 
 					{/* Followers count */}
-					<div className="text-xs text-[#666] flex items-center gap-1">
+					<div className="text-xs text-[#777] flex items-center gap-1">
 						<div className="flex gap-2 items-center">
 							<div className="flex">
 								<Image
-									src={StomanProfile}
+									src={Avatar1}
 									width="18"
 									height={18}
-									className="rounded-full border border-[#222]"
+									className="rounded-full border-2 border-[#222]"
 									alt="Stoman Profile"
 								/>
 								<Image
-									src={StomanProfile}
+									src={RonaldoAvatar}
 									width="18"
 									height={18}
-									className="rounded-full border border-[#222] -ml-2"
+									className="rounded-full border-2 border-[#222] -ml-[0.50rem]"
 									alt="Stoman Profile"
 								/>
 								<Image
-									src={StomanProfile}
+									src={NikeLogo}
 									width="18"
 									height={18}
-									className="rounded-full border border-[#222] -ml-2"
+									className="rounded-full border-2 border-[#222] -ml-[0.50rem]"
 									alt="Stoman Profile"
 								/>
 							</div>
@@ -84,6 +87,16 @@ const profile = () => {
 						</div>
 						<span>.</span>
 						<a href="#">stoman.me</a>
+					</div>
+
+					{/* Buttons */}
+					<div className="text-gray-300 flex justify-center text-xs gap-2 mt-3">
+						<button className="border border-[#333] px-12 py-1.5 rounded-lg">
+							Edit profile
+						</button>
+						<button className="border border-[#333] px-12 py-1.5 rounded-lg">
+							Share profile
+						</button>
 					</div>
 				</header>
 			</div>
