13918aeda374a10c06db7c643dcfcf39d8d2d785
realstoman <realstoman@gmail.com>
2023-07-14 17:46:15 +0430

    Activity component styles

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -24,6 +24,31 @@ const activity = () => {
 					activityTime="13m"
 					activityContent="When you're the only developer in a..."
 				/>
+
+				<div className="border border-[#222] w-full"></div>
+
+				<UserActivity
+					avatar={RonaldoAvatar}
+					activityType="reply"
+					username="zuck"
+					isVerified={true}
+					activityTime="2h"
+					activityContent="Threads is the exact copy of Twitter ..."
+					userReply="Exactly, if you look closely, even we didn't added the edit functionality like the Twitter app LOL"
+				/>
+
+				<div className="border border-[#222] w-full"></div>
+
+				<UserActivity
+					avatar={RonaldoAvatar}
+					activityType="follow"
+					username="taylorswift"
+					isVerified={true}
+					activityTime="18h"
+					activityContent="Followed you"
+				/>
+
+				<div className="border border-[#222] w-full"></div>
 			</div>
 		</PageWrapper>
 	);
--- components/activity/UserActivity.tsx
+++ components/activity/UserActivity.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import { FiHeart, FiMessageCircle, FiUser } from 'react-icons/fi';
+import BlueCheckMark from '@/public/avatars/blue-checkmark.png';
+
 interface ActivityType {
 	avatar: any;
 	activityType: string;
@@ -5,7 +9,6 @@ interface ActivityType {
 	isVerified?: boolean;
 	activityTime: string;
 	activityContent: string;
-	isReply?: boolean;
 	userReply?: string;
 }
 
@@ -16,12 +19,79 @@ const UserActivity = ({
 	isVerified,
 	activityTime,
 	activityContent,
-	isReply,
 	userReply,
 }: ActivityType) => {
 	return (
-		<div className="px-4 w-full my-4 clear-both">
-			<h1>{username}</h1>
+		<div className="px-4 w-full my-4 flex justify- items-start">
+			<div className="flex w-full gap-4">
+				<div className="flex items-start text-center">
+					<Image
+						src={avatar}
+						alt="User Avatar"
+						width={35}
+						height={35}
+						className="rounded-full"
+					/>
+					<div className="">
+						{activityType === 'like' ? (
+							<div className="bg-red-500 p-0.5 rounded-full -ml-2.5 mt-3 border-2 border-[#222]">
+								<FiHeart className=" fill-white text-xs" />
+							</div>
+						) : activityType === 'reply' ? (
+							<div className="bg-blue-400 p-0.5 rounded-full -ml-2.5 mt-3 border-2 border-[#222]">
+								<FiMessageCircle className=" fill-white text-xs" />
+							</div>
+						) : (
+							<div className="bg-purple-600 p-0.5 rounded-full -ml-2.5 mt-3 border-2 border-[#222]">
+								<FiUser className=" fill-white text-xs" />
+							</div>
+						)}
+					</div>
+				</div>
+				<div className="w-full">
+					<div className="">
+						<div className="flex items-center gap-3">
+							<div className="flex items-center gap-2">
+								<p className="text-md">{username}</p>
+
+								{isVerified ? (
+									<Image
+										src={BlueCheckMark}
+										alt="User"
+										width={14}
+										height={14}
+									/>
+								) : (
+									''
+								)}
+							</div>
+							<span className="text-xs text-[#666]">
+								{activityTime}
+							</span>
+						</div>
+						<div className="mt-0.5 text-sm text-[#777]">
+							{activityContent}
+							{activityType === 'reply' ? (
+								<p className="text-gray-200 mt-1">
+									{userReply}
+								</p>
+							) : (
+								''
+							)}
+						</div>
+					</div>
+					<div className="if-follow-btn"></div>
+				</div>
+			</div>
+			{activityType === 'follow' ? (
+				<div className="text-gray-300 flex justify-end text-xs">
+					<button className="border border-[#333] px-6 py-1 rounded-lg">
+						Follow
+					</button>
+				</div>
+			) : (
+				''
+			)}
 		</div>
 	);
 };
