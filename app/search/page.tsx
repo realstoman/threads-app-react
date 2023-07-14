889b432be0d21d20dde98197b8810292178ca95a
realstoman <realstoman@gmail.com>
2023-07-14 15:18:40 +0430

    Search page header

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,32 @@
 'use client';
 
-import React from 'react';
+import PageWrapper from '@/components/PageWrapper';
 
 const search = () => {
-	return <div>This is the search page</div>;
+	return (
+		<PageWrapper>
+			<div className="pt-5 pb-16 flex flex-col items-center overflow-y-scroll px-4">
+				{/* Header section */}
+				<header className=" w-full">
+					<h1 className="text-4xl font-semibold mb-2">Search</h1>
+					<div>
+						<form action="#">
+							<label htmlFor="search">
+								<input
+									type="search"
+									id="search"
+									placeholder="Search"
+									className="w-full border-none rounded-lg text-md px-3 py-2 bg-[#333] text-gray-200"
+								/>
+							</label>
+						</form>
+					</div>
+				</header>
+
+				{/* Dynamic Search Account */}
+			</div>
+		</PageWrapper>
+	);
 };
 
 export default search;
--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,6 +1,6 @@
 const PageWrapper = ({ children }: any) => {
 	return (
-		<main className="mx-auto min-h-screen max-w-lg sm:max-w-4xl">
+		<main className="mx-auto min-h-screen max-w-lg sm:max-w-4xl font-sans">
 			{children}
 		</main>
 	);
