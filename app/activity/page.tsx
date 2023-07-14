5c7fa0c77026449d77bd080d52d6310dcba4051e
realstoman <realstoman@gmail.com>
2023-07-14 16:50:16 +0430

    Activity page initial ui

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+'use client';
+
+import PageWrapper from '@/components/PageWrapper';
 
 const activity = () => {
-	return <div>activity</div>;
+	return (
+		<PageWrapper>
+			<div className="pt-5 pb-16 flex flex-col items-center overflow-y-scroll">
+				{/* Header section */}
+				<header className="w-full px-4">
+					<h1 className="text-4xl font-semibold mb-2">Activity</h1>
+					<div></div>
+				</header>
+			</div>
+		</PageWrapper>
+	);
 };
 
 export default activity;
