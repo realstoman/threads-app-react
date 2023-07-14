f0469b00353a56e831c05f1211ce7fc722f2674d
realstoman <realstoman@gmail.com>
2023-07-14 23:35:33 +0430

    Profile tabs

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,6 +7,7 @@ import StomanProfile from '@/public/avatars/stoman-avatar.jpg';
 import Avatar1 from '@/public/avatars/avatar-1.jpeg';
 import NikeLogo from '@/public/logo/nike-logo.jpg';
 import RonaldoAvatar from '@/public/avatars/ronaldo-avatar.jpg';
+import 'flowbite';
 
 const profile = () => {
 	return (
@@ -99,37 +100,51 @@ const profile = () => {
 						</button>
 					</div>
 				</header>
-				<div className="text-xs w-full text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 mt-3">
-					<ul className="flex justify-between items-center">
-						<li className="w-1/2 border-b border-gray-300">
-							<a
-								href="#"
-								className="inline-block p-2 rounded-t-lg text-gray-200"
+				<div className="text-xs w-full text-center mb-4 border-b border-gray-700 mt-3">
+					<ul
+						className="flex flex-wrap -mb-px text-xs font-medium text-center"
+						id="tab"
+						data-tabs-toggle="#tabContent"
+						role="tablist"
+					>
+						<li className="w-1/2 border-b" role="presentation">
+							<button
+								className="inline-block p-2 rounded-t-lg"
+								id="threads-tab"
+								data-tabs-target="#threads"
+								type="button"
+								role="tab"
+								aria-controls="threads"
+								aria-selected="true"
 							>
 								Threads
-							</a>
+							</button>
 						</li>
-						<li className="w-1/2">
-							<a
-								href="#"
-								className="inline-block p-2 text-[#777] rounded-t-lg"
-								aria-current="page"
+						<li className="w-1/2 border-b" role="presentation">
+							<button
+								className="inline-block p-2 border-transparent rounded-t-lg"
+								id="replies-tab"
+								data-tabs-target="#replies"
+								type="button"
+								role="tab"
+								aria-controls="replies"
+								aria-selected="false"
 							>
 								Replies
-							</a>
+							</button>
 						</li>
 					</ul>
 				</div>
-				<div id="myTabContent">
+				<div id="tabContent">
 					<div
 						className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-						id="profile"
+						id="threads"
 						role="tabpanel"
-						aria-labelledby="profile-tab"
+						aria-labelledby="threads-tab"
 					>
 						<p className="text-sm text-gray-500 dark:text-gray-400">
 							This is some placeholder content the{' '}
-							<strong className="font-medium text-gray-800 dark:text-white">
+							<strong className="text-gray-200">
 								Profile tabs associated content
 							</strong>
 							. Clicking another tab will toggle the visibility of
@@ -140,9 +155,9 @@ const profile = () => {
 					</div>
 					<div
 						className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-						id="dashboard"
+						id="replies"
 						role="tabpanel"
-						aria-labelledby="dashboard-tab"
+						aria-labelledby="replies-tab"
 					>
 						<p className="text-sm text-gray-500 dark:text-gray-400">
 							This is some placeholder content the{' '}
