253abd3177e04716e7f4f032335dd1d1ff786a98
realstoman <realstoman@gmail.com>
2023-07-16 23:34:54 +0430

    USer flowbite modal

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import Link from 'next/link';
 import { FiEdit, FiHeart, FiHome, FiSearch, FiUser } from 'react-icons/fi';
 import 'flowbite';
-import Create from './Create';
+import { Button, Modal } from 'flowbite-react';
+// import Create from './Create';
+import { useState } from 'react';
 
 const Navigation = () => {
+	const [openModal, setOpenModal] = useState<string | undefined>();
+	const props = { openModal, setOpenModal };
+
 	return (
 		<div className="fixed bottom-0 left-0 z-40 w-full h-16 bg-dark">
 			<div className="grid h-full max-w-lg sm:max-w-4xl grid-cols-5 mx-auto font-medium bg-[#111] text-[#666]">
@@ -20,6 +27,7 @@ const Navigation = () => {
 					<FiSearch className="hover:fill-white active:fill-white focus:fill-white text-2xl" />
 				</Link>
 				<button
+					onClick={() => props.setOpenModal('default')}
 					type="button"
 					data-modal-target="create-post-modal"
 					data-modal-toggle="create-post-modal"
@@ -42,7 +50,41 @@ const Navigation = () => {
 			</div>
 
 			{/* Create Post Modal */}
-			<Create />
+			<Modal
+				show={props.openModal === 'default'}
+				onClose={() => props.setOpenModal(undefined)}
+			>
+				<Modal.Header>Terms of Service</Modal.Header>
+				<Modal.Body>
+					<div className="space-y-6">
+						<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+							With less than a month to go before the European
+							Union enacts new consumer privacy laws for its
+							citizens, companies around the world are updating
+							their terms of service agreements to comply.
+						</p>
+						<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+							The European Union’s General Data Protection
+							Regulation (G.D.P.R.) goes into effect on May 25 and
+							is meant to ensure a common set of data rights in
+							the European Union. It requires organizations to
+							notify users as soon as possible of high-risk data
+							breaches that could personally affect them.
+						</p>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={() => props.setOpenModal(undefined)}>
+						I accept
+					</Button>
+					<Button
+						color="gray"
+						onClick={() => props.setOpenModal(undefined)}
+					>
+						Decline
+					</Button>
+				</Modal.Footer>
+			</Modal>
 		</div>
 	);
 };
