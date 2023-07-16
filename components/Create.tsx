5f0987cbba79c3e7f0236012432b758acb0456fa
realstoman <realstoman@gmail.com>
2023-07-16 14:31:26 +0430

    Create post modal ui

--- components/Create.tsx
+++ components/Create.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import 'flowbite';
-import { FiX } from 'react-icons/fi';
+import { FiPaperclip, FiX } from 'react-icons/fi';
+import Image from 'next/image';
+import StomanAvatar from '@/public/avatars/stoman-avatar.jpg';
 
 const Create = () => {
 	return (
@@ -8,114 +10,107 @@ const Create = () => {
 			id="create-post-modal"
 			tabIndex={-1}
 			aria-hidden="true"
-			className="fixed bottom-0 left-0 z-50 hidden w-full h-600 overflow-x-hidden overflow-y-auto p-0"
+			className="fixed bottom-0 left-0 z-50 hidden w-full  overflow-x-hidden overflow-y-auto p-0"
 		>
-			<div className="relative w-full max-w-md">
+			<div className="relative w-full h-full max-w-md">
 				{/* <!-- Modal content --> */}
-				<div className="relative rounded-lg bg-[#222] text-gray-100">
-					<header className="border-b border-[#333]">
+				<div className="relative rounded-t-lg h-[500px] bg-[#222] text-gray-100">
+					<div className="border-b border-[#333]">
 						<div className="grid grid-cols-8 py-3 px-4">
 							<button
 								type="button"
-								className="text-left col-span-1 hover:text-gray-900 rounded-lg  inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white text-xs"
+								className="absolute text-left pt-1 col-span-1 rounded-lg inline-flex justify-center items-center text-[#bbb] text-xs"
 								data-modal-hide="create-post-modal"
 							>
 								Cancel
 								<span className="sr-only">Close modal</span>
 							</button>
-							<h1 className="col-span-7 -ml-8 text-center font-medium">
+							<h1 className="col-span-7 text-center font-medium">
 								New thread
 							</h1>
 						</div>
-					</header>
+					</div>
 
-					<div className="px-4 py-6 flex w-full">
-						<div></div>
-						<div className="flex w-full">
-							<form className="space-y-6" action="#">
-								<div>
-									<label
-										htmlFor="email"
-										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-									>
-										Your email
-									</label>
-									<input
-										type="email"
-										name="email"
-										id="email"
-										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-										placeholder="name@company.com"
-										required
-									></input>
-								</div>
-								<div>
-									<label
-										htmlFor="password"
-										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-									>
-										Your password
-									</label>
-									<input
-										type="password"
-										name="password"
-										id="password"
-										placeholder="••••••••"
-										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-										required
-									></input>
-								</div>
-								<div className="flex justify-between">
-									<div className="flex items-start">
-										<div className="flex items-center h-5">
-											<input
-												id="remember"
-												type="checkbox"
-												value=""
-												className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-												required
-											></input>
+					<form className="space-y-6" action="#">
+						<div className="px-4 py-6 flex w-full gap-2">
+							<div className=" ml-2">
+								<div className="relative border-l-2 border-[#333] border-opacity-70 ml-2">
+									<div className="flex -ml-7 flex-col w-14 h-36 max-h-36 justify-between items-center">
+										<div>
+											<Image
+												src={StomanAvatar}
+												width={35}
+												height={35}
+												alt="Account Avatar"
+												className="rounded-full"
+											/>
 										</div>
-										<label
-											htmlFor="remember"
-											className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-										>
-											Remember me
-										</label>
+
+										<div className="flex flex-col justify-center items-center">
+											<Image
+												src={StomanAvatar}
+												width={16}
+												height={16}
+												alt="Account Avatar"
+												className="rounded-full opacity-30"
+											/>
+										</div>
 									</div>
-									<a
-										href="#"
-										className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-									>
-										Lost Password?
-									</a>
+								</div>
+							</div>
+							<div className="flex w-full h-full">
+								<div className="space-y-6">
+									<div>
+										<input
+											type="text"
+											name="post"
+											id="post"
+											className="bg-transparent text-xs border-none w-full focus:border-none ring-0 p-0 focus:ring-0 active:ring-0"
+											required
+										></input>
+									</div>
+
+									<button type="button">
+										<FiPaperclip className="text-[#777]" />
+									</button>
+
+									<div>
+										<input
+											type="text"
+											name="post"
+											id="post"
+											className="bg-transparent text-xs border-none w-full focus:border-none ring-0 p-0 focus:ring-0 active:ring-0"
+											required
+											placeholder="Add to thread"
+										></input>
+									</div>
 								</div>
 								<button
-									type="submit"
-									className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+									type="button"
+									className=" mt-0 right-2.5 text-sm w-8 h-8 ml-auto inline-flex justify-center items-center "
+									data-modal-hide="create-post-modal"
 								>
-									Login to your account
+									<FiX className="w-4 h-4 text-[#777]" />
+									<span className="sr-only">Close modal</span>
 								</button>
-								<div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-									Not registered?{' '}
-									<a
-										href="#"
-										className="text-blue-700 hover:underline dark:text-blue-500"
-									>
-										Create account
-									</a>
-								</div>
-							</form>
+							</div>
+						</div>
+
+						{/* Footer */}
+						<div className="flex justify-between items-center px-4 bottom-0 right-0 left-0 fixed pb-3">
+							<div className="text-xs text-[#666]">
+								<p>Anyone can reply</p>
+							</div>
 							<button
-								type="button"
-								className="absolute mt-2 right-2.5 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+								type="submit"
+								className="text-left col-span-1 rounded-lg inline-flex justify-center items-center text-blue-400 text-sm font-medium"
 								data-modal-hide="create-post-modal"
 							>
-								<FiX className="w-6 h-6" />
-								<span className="sr-only">Close modal</span>
+								Post
+								<span className="sr-only">Post</span>
 							</button>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
