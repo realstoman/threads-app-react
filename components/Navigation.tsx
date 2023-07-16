d26914bffc3408039866ea34a25af7582ead0b00
realstoman <realstoman@gmail.com>
2023-07-16 11:37:07 +0430

    Add flowbite react

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import { FiEdit, FiHeart, FiHome, FiSearch, FiUser } from 'react-icons/fi';
+import 'flowbite';
+import { Modal } from 'flowbite';
 
 const Navigation = () => {
 	return (
-		<div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-dark">
+		<div className="fixed bottom-0 left-0 z-40 w-full h-16 bg-dark">
 			<div className="grid h-full max-w-lg sm:max-w-4xl grid-cols-5 mx-auto font-medium bg-[#111] text-[#666]">
 				<Link
 					href="/"
@@ -18,9 +20,9 @@ const Navigation = () => {
 					<FiSearch className="hover:fill-white active:fill-white focus:fill-white text-2xl" />
 				</Link>
 				<button
-					data-modal-target="medium-modal"
-					data-modal-toggle="medium-modal"
 					type="button"
+					data-modal-target="create-post-modal"
+					data-modal-toggle="create-post-modal"
 					className="inline-flex flex-col items-center justify-center px-5 group"
 				>
 					<FiEdit className="hover:fill-white active:fill-white focus:fill-white text-2xl" />
@@ -41,76 +43,114 @@ const Navigation = () => {
 
 			{/* Create Modal */}
 			<div
-				id="medium-modal"
+				id="create-post-modal"
 				tabIndex={-1}
+				aria-hidden="true"
 				className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
 			>
-				<div className="relative w-full max-w-lg max-h-full">
+				<div className="relative w-full max-w-md max-h-full">
 					{/* <!-- Modal content --> */}
 					<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-						{/* <!-- Modal header --> */}
-						<div className="flex items-center justify-between p-5 border-b rounded-t dark:border-gray-600">
-							<h3 className="text-xl font-medium text-gray-900 dark:text-white">
-								Default modal
+						<button
+							type="button"
+							className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+							data-modal-hide="create-post-modal"
+						>
+							<svg
+								className="w-3 h-3"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+								/>
+							</svg>
+							<span className="sr-only">Close modal</span>
+						</button>
+						<div className="px-6 py-6 lg:px-8">
+							<h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+								Sign in to our platform
 							</h3>
-							<button
-								type="button"
-								className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-								data-modal-hide="medium-modal"
-							>
-								<svg
-									className="w-3 h-3"
-									aria-hidden="true"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 14 14"
+							<form className="space-y-6" action="#">
+								<div>
+									<label
+										htmlFor="email"
+										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+									>
+										Your email
+									</label>
+									<input
+										type="email"
+										name="email"
+										id="email"
+										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+										placeholder="name@company.com"
+										required
+									></input>
+								</div>
+								<div>
+									<label
+										htmlFor="password"
+										className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+									>
+										Your password
+									</label>
+									<input
+										type="password"
+										name="password"
+										id="password"
+										placeholder="••••••••"
+										className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+										required
+									></input>
+								</div>
+								<div className="flex justify-between">
+									<div className="flex items-start">
+										<div className="flex items-center h-5">
+											<input
+												id="remember"
+												type="checkbox"
+												value=""
+												className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+												required
+											></input>
+										</div>
+										<label
+											htmlFor="remember"
+											className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+										>
+											Remember me
+										</label>
+									</div>
+									<a
+										href="#"
+										className="text-sm text-blue-700 hover:underline dark:text-blue-500"
+									>
+										Lost Password?
+									</a>
+								</div>
+								<button
+									type="submit"
+									className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 								>
-									<path
-										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-									/>
-								</svg>
-								<span className="sr-only">Close modal</span>
-							</button>
-						</div>
-						{/* <!-- Modal body --> */}
-						<div className="p-6 space-y-6">
-							<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-								With less than a month to go before the European
-								Union enacts new consumer privacy laws for its
-								citizens, companies around the world are
-								updating their terms of service agreements to
-								comply.
-							</p>
-							<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-								The European Union’s General Data Protection
-								Regulation (G.D.P.R.) goes into effect on May 25
-								and is meant to ensure a common set of data
-								rights in the European Union. It requires
-								organizations to notify users as soon as
-								possible of high-risk data breaches that could
-								personally affect them.
-							</p>
-						</div>
-						{/*  */}
-						<div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-							<button
-								data-modal-hide="medium-modal"
-								type="button"
-								className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-							>
-								I accept
-							</button>
-							<button
-								data-modal-hide="medium-modal"
-								type="button"
-								className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-							>
-								Decline
-							</button>
+									Login to your account
+								</button>
+								<div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+									Not registered?{' '}
+									<a
+										href="#"
+										className="text-blue-700 hover:underline dark:text-blue-500"
+									>
+										Create account
+									</a>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./node_modules/flowbite/**/*.js',
+		'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
 	],
 	theme: {
 		extend: {
