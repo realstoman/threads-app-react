97e4f8dbc4e57ce54be0601b0c53d402b6be40cb
realstoman <realstoman@gmail.com>
2023-07-21 23:00:49 +0430

    Create types file

---
+++ types/AppTypes.tsx
@@ -0,0 +1,9 @@
+// Suggested Accounts Types
+interface SuggestedAccountType {
+	avatar: any;
+	username: string;
+	isVerified: boolean;
+	fullName: string;
+}
+
+export default SuggestedAccountType;
